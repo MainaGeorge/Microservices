49dc3e290b0fd47284c27833195d2e742ba905f5
George Maina <nuriszka@gmail.com>
2021-11-30 15:56:02 +0100

    adding basket repository pattern

--- src/Services/Basket/Startup.cs
+++ src/Services/Basket/Startup.cs
@@ -16,11 +16,11 @@ namespace Basket.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+
             services.AddStackExchangeRedisCache(opt =>
                 {
                     opt.Configuration = Configuration["CacheSettings:ConnectionString"];
@@ -31,7 +31,6 @@ namespace Basket.API
                 });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
